a95a7041eeaadfd2b6f26dca4a6723e8638fd019
Lyse13 <lysettemouandeu@gmail.com>
2025-11-07 10:29:06 +0100

    Project setup

--- site/src/jsMain/kotlin/com/lysette/san/pages/Index.kt
+++ site/src/jsMain/kotlin/com/lysette/san/pages/Index.kt
@@ -1,26 +1,22 @@
 package com.lysette.san.pages
 
-import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import com.lysette.san.sections.MainSection
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
-import org.jetbrains.compose.web.css.vh
-import org.jetbrains.compose.web.dom.Text
-import com.varabyte.kobweb.worker.rememberWorker
-import com.lysette.san.worker.EchoWorker
 
 @Page
 @Composable
 fun HomePage() {
-    val worker = rememberWorker { EchoWorker { output -> console.log("Echoed: $output") } }
-    LaunchedEffect(Unit) {
-        worker.postInput("Hello, worker!")
-    }
-
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        MainSection()
     }
 }
---
+++ site/src/jsMain/kotlin/com/lysette/san/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.lysette.san.util
+
+object Constants {
+    const val SECTION_WIDTH = 1920
+}
+
+object Res {
+    object Image {
+        const val background = "background.svg"
+    }
+}
